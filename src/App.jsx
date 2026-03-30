d0b97960bd78d4fb6b422601651a787617972471
loucorbally <louisecorbally1@gmail.com>
2026-03-30 11:31:44 +0100

    Add explanatory comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import AuthPage from './pages/AuthPage'
 import Dashboard from './pages/Dashboard'
 
 export default function App() {
+  // stores the currently logged in user
+  // null means no user is logged in
   const [user, setUser] = useState(null)
 
+  //if no user is logged in, dislay the login / register page
   if (!user) return <AuthPage onAuth={setUser} />
+
+  //if user is logged in, display the dashboard
   return <Dashboard user={user} onLogout={() => setUser(null)} />
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// render the root App component into the HTML element with id="root"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
